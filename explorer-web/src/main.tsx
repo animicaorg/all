ea4737a37538a082fb9b633b7a450077a663d1a6
Alexander Lehman <animicaorg@gmail.com>
2025-11-26 01:44:11 -0800

    Fix router nesting and clean style tag

--- explorer-web/src/main.tsx
+++ explorer-web/src/main.tsx
@@ -1,6 +1,5 @@
 import React, { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App";
 
 // Global styles
@@ -37,9 +36,7 @@ function mount() {
   const root = createRoot(container);
   root.render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </StrictMode>
   );
 }
