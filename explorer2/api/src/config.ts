acabfad17a07999f8039f20cb32154e40222e0e1
Alexander Lehman <animicaorg@gmail.com>
2025-12-20 01:27:24 -0600

    Move explorer2 web to port 3001

--- explorer2/api/src/config.ts
+++ explorer2/api/src/config.ts
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const config = {
-  port: Number(process.env.EXPLORER2_PORT || process.env.PORT || 3001),
+  port: Number(process.env.EXPLORER2_PORT || process.env.PORT || 8081),
   rpcUrl: process.env.EXPLORER2_RPC_URL || 'http://127.0.0.1:8545/rpc',
   corsOrigin: process.env.EXPLORER2_CORS_ORIGIN || '*',
   logLevel: process.env.EXPLORER2_LOG_LEVEL || 'info',
